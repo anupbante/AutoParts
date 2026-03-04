0047bfad6c590b5b74b66ae745128ed107271e75
ANUP BANTE <anup.bante.dpu@gmail.com>
2026-03-04 19:47:05 +0530

    modified parts controller and created order controller

--- src/main/java/com/autoparts/controller/OrderedPartsController.java
+++ src/main/java/com/autoparts/controller/OrderedPartsController.java
@@ -1,14 +1,16 @@
 package com.autoparts.controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 @RestController
-@RequestMapping("parts")
-public class PartsController {
+@RequestMapping("orderedparts")
+public class OrderedPartsController {
 
-
-    @GetMapping("all")
+    @GetMapping("order")
     private String getAllParts() {
-        return "part a, part b, part c";
+        return "order a, order b, order c";
     }
 }
