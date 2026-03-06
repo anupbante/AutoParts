ed45f4b46220403c168ba56beee43f60c7d07a38
ANUP BANTE <anup.bante.dpu@gmail.com>
2026-03-06 15:13:59 +0530

    initial project configuration changes

---
+++ src/main/java/com/autoparts/test/AdditionOfTwoNumbers.java
@@ -0,0 +1,12 @@
+package com.autoparts.test;
+
+public class AdditionOfTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 2;
+
+        int total = num1 + num2;
+
+        System.out.println("Total : " + total);
+    }
+}
---
+++ src/main/java/com/autoparts/test/MultiplicationOfTwoNumber.java
@@ -0,0 +1,12 @@
+package com.autoparts.test;
+
+public class MultiplicationOfTwoNumber {
+public static void main(String[] args) {
+    int num1 = 4;
+    int num2 = 5;
+
+    int total = num1 * num2;
+
+    System.out.println("Total : " + total);
+}
+}
