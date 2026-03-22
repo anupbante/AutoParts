ff3980ff32508d8e8989da1464e50040a7a118c4
ANUP BANTE <anup.bante.dpu@gmail.com>
2026-03-22 20:43:34 +0530

    loop

--- src/main/java/com/autoparts/test/collections/CollectionTest.java
+++ src/main/java/com/autoparts/test/collections/CollectionTest.java
@@ -31,20 +31,64 @@ public class CollectionTest {
         namesArrayList.add("abc");
         namesArrayList.add("rewati");
 
-       for (int i = 0; i < namesArrayList.size(); i++) {
+        for (int i = 0; i < namesArrayList.size(); i++) {
             String name = namesArrayList.get(i);
             System.out.println("list of names in arraylist: " + name);
         }
     }
 
-    /**
+
+    /*
      * This method checks the positive number
      * present in the given number list.
      */
 
     public static void isPositive() {
-        ArrayList<Boolean> numberList = new ArrayList<>();
+
+        ArrayList<Integer> numberList = new ArrayList<>();
+        numberList.add(11);
+        numberList.add(53);
+        numberList.add(45);
+        for (int i = 0; i < numberList.size(); i++) {
+            int num = numberList.get(i);
+            if (num > 0) ;
+            System.out.println("check number ::" + num);
+        }
     }
+
+    public static void isEven() {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(11);
+        numbers.add(53);
+        numbers.add(45);
+        for (int i = 0; i < numbers.size(); i++) {
+            int num1 = numbers.get(i);
+            if (num1 % 2 == 0) ;
+            System.out.println(num1 + "isEven");
+
+        }
+    }
+
+    public static void isNamePresent() {
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("anup");
+        names.add("abc");
+        names.add("rewati");
+
+        for (int i = 0; i < names.size(); i++) {
+            String name = names.get(i);
+            if (name.equalsIgnoreCase("abc")) {
+                System.out.println(name + "is present");
+            }
+        }
+    }
+
+}
+
+
+
+
 
     /* TO-DO:
         1. isEven()
@@ -52,5 +96,7 @@ public class CollectionTest {
         3. isNamePresent()
     */
 
-
-}
+/*
+*This method checks the even number
+     * present in the given number list.
+ */
