c87e078444a50a1e43fff2a48b8817912031f0f7
ANUP BANTE <anup.bante.dpu@gmail.com>
2026-03-23 20:03:49 +0530

    loop

--- src/main/java/com/autoparts/test/collections/HashMapTest.java
+++ src/main/java/com/autoparts/test/collections/HashMapTest.java
@@ -12,6 +12,7 @@ import java.util.HashMap;
 
        3. HashSet => sequential manner me data store karta hai and duplicates
        allow nahi karta.
+
            Ex. "anup", "abc", "rewati" => ye elements present hai hashset
            me, ab agar mai isme firse "anup" add karunga toh ye usko
            skip kar dega and hashset ka size 3 hi rahega
@@ -23,18 +24,30 @@ public class HashMapTest {
 
         nameAge.put("rewati", 31);
         nameAge.put("anup", 30);
-        nameAge.put("abc", 50);
+        nameAge.put("abc", 60);
         nameAge.put("xyz", 10);
+        /*
+        1)humne joo phele for (int i = 0; i < nameAge.size(); i++) use kiya tha wo index-based loop hai.
+        Lekin HashMap index-based nahi hota.
+        HashMap ek key–value pair data structure hai. Har element ek unique key se access hota hai, index se nahi.
+        example: jaise idhar anup, rewati key hai aur uski value 31 aur 30 hai.
+        for (HashMap.Entry<String, Integer> entry : nameAge.entrySet())
 
-        for (int i = 0; i < nameAge.size(); i++) {
-            int age = nameAge.get("rewati");
+        2)Index based me har element ek position (index) se access hota hai.
+        Index based Array aur ArrayList hote hain.
+        example:(int i = 0; i < nameAge.size(); i++)
+        */
+
+        for (HashMap.Entry<String, Integer> entry : nameAge.entrySet()) {
+            String name = entry.getKey();
+            int age = entry.getValue();
 
             if (age < 18) {
-                System.out.println("rewati is an child");
-            } else if (age > 18) {
-                System.out.println("rewati is an adult");
-            } else {
-                System.out.println("rewati is an elder");
+                System.out.println(name +  "is a child");
+            } else if (age > 18 && age < 50 ) {
+                System.out.println(name +  "is a adult");
+            } else  {
+                System.out.println(name +  "is a elder");
             }
 
         }
