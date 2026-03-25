a7b749b145fe69dd45841732753d0e5f1de66bea
rewati.chandrol <singhraman834@gmail.com>
2026-03-25 20:38:40 +0530

    FilterDuplicates assignment

--- src/main/java/com/autoparts/test/collections/assignments/FilterDuplicatesMain.java
+++ src/main/java/com/autoparts/test/collections/assignments/FilterDuplicatesMain.java
@@ -1,5 +1,10 @@
 package com.autoparts.test.collections.assignments;
 
+
+/*
+The goal is to create the object of FilterDuplicates class and call the method which is filtering
+the duplicate numbers, get the returned value here in main() method and then print them.
+ */
 public class FilterDuplicatesMain {
     public static void main(String[] args) {
 
