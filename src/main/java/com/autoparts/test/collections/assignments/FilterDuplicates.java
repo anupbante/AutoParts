ee332c37abb89c8a8f638747ea3c68157b478132
rewati.chandrol <singhraman834@gmail.com>
2026-03-25 20:35:36 +0530

    FilterDuplicates assignment

---
+++ src/main/java/com/autoparts/test/collections/assignments/FilterDuplicates.java
@@ -0,0 +1,16 @@
+package com.autoparts.test.collections.assignments;
+
+
+/*
+This class finds the duplicate numbers in a given numbers list, stores them separately
+and returns back to the caller.
+
+Input: {1, 3, 1, 6, 7, 10, 7, 55, 79, 79}
+Output: {1, 7, 79}
+
+This is NOT just about printing the duplicate numbers, but to identify them and store them and return them, and then
+print them.
+ */
+public class FilterDuplicates {
+
+}
---
+++ src/main/java/com/autoparts/test/collections/assignments/FilterDuplicatesMain.java
@@ -0,0 +1,7 @@
+package com.autoparts.test.collections.assignments;
+
+public class FilterDuplicatesMain {
+    public static void main(String[] args) {
+
+    }
+}
